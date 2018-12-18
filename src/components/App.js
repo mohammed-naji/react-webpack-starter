dcd32561be96c9339965b00082f4ed0d73a523bb
Mohammed Naji <m.naji1512@gmail.com>
2018-12-18 06:08:13 +0200

    First version of this boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the first app using WEBPACK and BABEL without create-react-app</h1>
+      </div>
+    )
+    
+  }
+}
+export default App; 
